961fb1d70871c8c79d9491820981419f683bf288
13bscsmahmad <13bscsmahmad@seecs.edu.pk>
2017-04-03 01:48:22 +0500

    Script to convert libsvm data to .csv

---
+++ xgboost/xgboost-IPCA/libsvm2csv.sh
@@ -0,0 +1,3 @@
+python ../../IPCA/phraug/libsvm2csv.py ../test_svmLightPCA_test_numeric.txt ./test_numeric_pca-100_csv.csv 100
+
+python ../../IPCA/phraug/libsvm2csv.py ../finalPCACompleteTrainNumeric.txt ./train_numeric_pca-100_csv.csv 100
