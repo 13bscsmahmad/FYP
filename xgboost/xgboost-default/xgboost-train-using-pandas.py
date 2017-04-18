ecc1320d8747a3cdc3461b585bc0361df1a6a81c
13bscsmahmad <13bscsmahmad@seecs.edu.pk>
2017-04-18 19:41:58 +0500

    xgboost on simple train_numeric using pandas and default xgbparams

---
+++ xgboost/xgboost-default/xgboost-train-using-pandas.py
@@ -0,0 +1,50 @@
+'''
+This script uses the dataset train_numeric.
+Unlike the other script,
+this does NOT split the data into 70:30 train:test.
+It trains the COMPLETE train_numeric dataset through the xgboost algorithm,
+and saves the model.
+
+Uses pandas library
+'''
+
+import numpy as np
+import xgboost as xgb
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pickle
+import pandas as pd
+
+TRAIN_DATA = "../../Dataset/id-based-features-and-numeric-train.csv"
+
+dataset = pd.read_csv(TRAIN_DATA)
+print("dataset shape: ")
+print(dataset.shape)
+
+# Get labels and ids from the file
+y = dataset.Response.ravel()
+ids = dataset.Id.ravel()
+
+# Drop Id and Response columns from the training dataset
+dataset = dataset.drop('Id', 1)
+dataset = dataset.drop('Response', 1)
+
+
+# Convert dataset to numpy array
+dataset = np.array(dataset)
+
+#print shape of training_dataset
+print("dataset shape: ")
+print(dataset.shape)
+
+dtrain = xgb.DMatrix(dataset, label=y)
+print ("training data...")
+
+#Train the model
+trained_model = xgb.train(dtrain=dtrain, verbose_eval=True)
+
+#Save the model
+trained_model.save_model("xgboost-numeric-and-features-engineered-faron-model")
+
+print("Saved model.")
