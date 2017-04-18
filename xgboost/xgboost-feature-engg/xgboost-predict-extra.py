ac7a9b6e273ec0dad86a9d78079a264d14be4c49
13bscsmahmad <13bscsmahmad@seecs.edu.pk>
2017-04-18 18:30:10 +0500

    Fix file pathnames and add printing statements.

--- xgboost/xgboost-feature-engg/xgboost-predict-extra.py
+++ xgboost/xgboost-feature-engg/xgboost-predict-extra.py
@@ -9,24 +9,31 @@ import numpy as np
 import xgboost as xgb
 
 MODEL_NAME = "xgboost-numeric-and-features-engineered-faron-model"
-TEST_DATA = "../../Dataset/id-based-features-test.csv"
+TEST_DATA = "../../Dataset/id-based-features-and-numeric-test.csv"
 
 testing_dataset = pd.read_csv(TEST_DATA)
+print("testing_dataset shape: ")
+print(testing_dataset.shape)
 
+print("Removing Id column...")
 ids = testing_dataset.Id.ravel()
 testing_dataset.drop('Id', 1) # drop the Id column from the dataframe
 
 ## Convert training_dataset to numpy array
 testing_dataset = np.array(testing_dataset)
+print("testing_dataset numpy array's shape:")
+print(testing_dataset.shape)
 
 # load model from file
+print("Loading saved model...")
 bst = xgb.Booster({'nthread':4}) #init model
-bst.load_model("xgboost-numeric-and-features-engineered-faron-model") # load data
+bst.load_model(MODEL_NAME) # load data
 
 #Data to test in DMatrix form
 dtest = xgb.DMatrix(testing_dataset)
 
 # make predictions for test data
+print("Predicting...")
 y_pred = bst.predict(dtest)
 #predictions = [int(round(value)) for value in y_pred]
 
