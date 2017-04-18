c92eae1f7122971095d362501a42b34d5b62a459
13bscsmahmad <13bscsmahmad@seecs.edu.pk>
2017-04-18 18:22:19 +0500

    Fix file path

--- xgboost/xgboost-feature-engg/xgboost-train-extra.py
+++ xgboost/xgboost-feature-engg/xgboost-train-extra.py
@@ -14,7 +14,7 @@ import xgboost as xgb
 
 DATA_DIR = "../../Dataset"
 
-ID_BASED_AND_NUMERIC_FEATURES_TRAIN = "../../Dataset/id-based-features-train.csv"
+ID_BASED_AND_NUMERIC_FEATURES_TRAIN = "../../Dataset/id-based-features-and-numeric-train.csv"
 
 # Read train dataset
 training_dataset = pd.read_csv(ID_BASED_AND_NUMERIC_FEATURES_TRAIN)
