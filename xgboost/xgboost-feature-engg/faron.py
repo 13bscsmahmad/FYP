32fb05d42fade5d444f34b5dd974e072325fbd49
13bscsmahmad <13bscsmahmad@seecs.edu.pk>
2017-05-10 03:38:01 +0500

    Fix

--- xgboost/xgboost-feature-engg/faron.py
+++ xgboost/xgboost-feature-engg/faron.py
@@ -12,59 +12,15 @@ ID_COLUMN = 'Id'
 TARGET_COLUMN = 'Response'
 
 SEED = 0
-CHUNKSIZE = 50000
-NROWS = 250000
+# CHUNKSIZE = 50000
+# NROWS = 250000
 
-TRAIN_NUMERIC = "{0}/train_numeric.csv".format(DATA_DIR)
-TRAIN_DATE = "{0}/train_date.csv".format(DATA_DIR)
+TRAIN_NUMERIC = "{0}/id-based-features-and-numeric-train.csv".format(DATA_DIR)
+TEST_NUMERIC = "{0}/id-based-features-and-numeric-test.csv".format(DATA_DIR)
 
-TEST_NUMERIC = "{0}/test_numeric.csv".format(DATA_DIR)
-TEST_DATE = "{0}/test_date.csv".format(DATA_DIR)
-
-FILENAME = "etimelhoods"
 
 train = pd.read_csv(TRAIN_NUMERIC)
 test = pd.read_csv(TEST_NUMERIC)
-
-train["StartTime"] = -1
-test["StartTime"] = -1
-
-
-nrows = 0
-tr = pd.read_csv(TRAIN_DATE)
-te = pd.read_csv(TEST_DATE)
-
-
-
-feats = np.setdiff1d(tr.columns, [ID_COLUMN]) # get all features i.e. remove ID for all columns
-
-stime_tr = tr[feats].min(axis=1).values
-stime_te = te[feats].min(axis=1).values
-
-train.loc[train.Id.isin(tr.Id), 'StartTime'] = stime_tr
-test.loc[test.Id.isin(te.Id), 'StartTime'] = stime_te
-
-    # nrows += CHUNKSIZE
-#     if nrows >= NROWS:
-#         break
-
-
-ntrain = train.shape[0]
-train_test = pd.concat((train, test)).reset_index(drop=True).reset_index(drop=False)
-
-train_test['0_¯\_(ツ)_/¯_1'] = train_test[ID_COLUMN].diff().fillna(9999999).astype(int)
-
-train_test['0_¯\_(ツ)_/¯_2'] = train_test[ID_COLUMN].iloc[::-1].diff().fillna(9999999).astype(int)
-
-train_test = train_test.sort_values(by=['StartTime', 'Id'], ascending=True)
-
-train_test['0_¯\_(ツ)_/¯_3'] = train_test[ID_COLUMN].diff().fillna(9999999).astype(int)
-train_test['0_¯\_(ツ)_/¯_4'] = train_test[ID_COLUMN].iloc[::-1].diff().fillna(9999999).astype(int)
-
-# print(train_test.loc[pd.isnull(train_test["StartTime"])])
-
-train_test = train_test.sort_values(by=['index']).drop(['index'], axis=1)
-train = train_test.iloc[:ntrain, :]
 
 #print(train_test)
 
@@ -106,14 +62,12 @@ booster_model = xgb.train(xgb_params, dtrain)
 Predict and write to file
 '''
 
-test_data = train_test.iloc[ntrain:, :]
-test_data = np.array(test_data[features])
-dtest = xgb.DMatrix(test_data)
+iidd = test_data.Id.ravel()
+test = test.drop('Id', 1)
+test = np.array(test[features])
+dtest = xgb.DMatrix(test)
 y_pred = booster_model.predict(dtest) # returns numpy array
 
-iidd = test_data.Id.ravel()
-test_data = test_data.drop('Id', 1)
-test_data = np.array(test_data)
 predictions = [int(round(value)) for value in y_pred]
 IDs_in_height = np.array(iidd)[np.newaxis].T
 predictions_in_height = np.array(predictions)[np.newaxis].T
