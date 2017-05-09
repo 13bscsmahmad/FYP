9628b5386a531880446411b4f853b07fc4972c5d
13bscsmahmad <13bscsmahmad@seecs.edu.pk>
2017-05-10 02:28:48 +0500

    Fix

--- xgboost/xgboost-feature-engg/faron.py
+++ xgboost/xgboost-feature-engg/faron.py
@@ -35,14 +35,14 @@ tr = pd.read_csv(TRAIN_DATE)
 te = pd.read_csv(TEST_DATE)
 
 
+for tr, te in zip(pd.read_csv(TRAIN_DATE), pd.read_csv(TEST_DATE):
+    feats = np.setdiff1d(tr.columns, [ID_COLUMN]) # get all features i.e. remove ID for all columns
 
-feats = np.setdiff1d(tr.columns, [ID_COLUMN]) # get all features i.e. remove ID for all columns
+    stime_tr = tr[feats].min(axis=1).values
+    stime_te = te[feats].min(axis=1).values
 
-stime_tr = tr[feats].min(axis=1).values
-stime_te = te[feats].min(axis=1).values
-
-train.loc[train.Id.isin(tr.Id), 'StartTime'] = stime_tr
-test.loc[test.Id.isin(te.Id), 'StartTime'] = stime_te
+    train.loc[train.Id.isin(tr.Id), 'StartTime'] = stime_tr
+    test.loc[test.Id.isin(te.Id), 'StartTime'] = stime_te
 
     # nrows += CHUNKSIZE
 #     if nrows >= NROWS:
